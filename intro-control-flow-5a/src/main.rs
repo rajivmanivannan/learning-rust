e97a2da0eb2904e1acf32d010398ea24036deec6
rajivmanivannan <rajiv.techconsultant@gmail.com>
2021-12-17 14:15:11 +0530

    control flow

---
+++ intro-control-flow-5a/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+// If-else-if ladder
+// If NUM is 1, Print you won!
+
+let num =2;
+
+if num ==1 { 
+    println!("You won!");
+} else if num == 2 {
+    println!("Try again");
+} else{
+    println!("You lost");
+}
+
+
+}
